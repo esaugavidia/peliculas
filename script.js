acafc662f2dc5afed52bd0282a7f49009aab1ad2
esaugavidia <143225183+esaugavidia@users.noreply.github.com>
2023-11-01 10:05:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let loadMoreBtn1 = document.querySelector('#load-more-1');
+let currentItem1 = 4;
+
+loadMoreBtn1.onclick = () => {
+    let boxes = [...currentItem1.document.querySelectorAll(
+        '.box-container-1 .box-1'
+    )];
+    for(var i = currentItem1; i < currentItem1 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem1 += 4;
+    if(currentItem1 >= boxes.length) {
+        loadMoreBtn1.style.display = 'none';
+        
+        }
+    }
+
